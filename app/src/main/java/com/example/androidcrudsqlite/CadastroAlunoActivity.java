7859962117020b39ec037e3091e13635e69877c4
djairb <djair.batista@outlook.com>
2022-10-22 12:09:33 -0300

    Validação de dados na tela de cadastro

--- app/src/main/java/com/example/androidcrudsqlite/CadastroAlunoActivity.java
+++ app/src/main/java/com/example/androidcrudsqlite/CadastroAlunoActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class CadastroAlunoActivity extends AppCompatActivity {
 
@@ -27,13 +28,86 @@ public class CadastroAlunoActivity extends AppCompatActivity {
         salvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Aluno aluno = new Aluno();
-                aluno.setCpf(cpf.getText().toString());
-                aluno.setNome(nome.getText().toString());
-                aluno.setTelefone(telefone.getText().toString());
+                if(validarCampos()){
+                    montarAluno();
+
+                }
 
             }
         });
 
     }
+    public void montarAluno(){
+        Aluno aluno = new Aluno();
+        aluno.setCpf(cpf.getText().toString());
+        aluno.setNome(nome.getText().toString());
+        aluno.setTelefone(telefone.getText().toString());
+        Toast.makeText(this, "Suscesso ao criar a classe aluno.", Toast.LENGTH_LONG).show();
+    }
+
+
+    private boolean validarCampos(){
+
+        boolean erro = true;
+        if (validarCpf()){
+            erro = false;
+        }
+        if (validarNome()){
+            erro = false;
+        }
+        if(validarTelefone()){
+            erro = false;
+        }
+        return erro;
+
+
+
+
+    }
+
+    private boolean validarCpf(){
+
+        boolean erro = false;
+        String cpfString = cpf.getText().toString().trim();
+        if(cpfString.isEmpty()){
+            erro = true;
+            cpf.setError("Campo em branco");
+        }else if(cpfString.length() != 11){
+            erro = true;
+            cpf.setError("Cpf não em 11 dígitos");
+        }else if(!cpfString.matches("[0-9]+")){
+            erro = true;
+            cpf.setError("Cpf não contem apenas numeros");
+        }
+        return erro;
+
+    }
+
+    private boolean validarNome(){
+
+        boolean erro = false;
+        String nomeString = nome.getText().toString().trim();
+        if(nomeString.isEmpty()){
+            erro = true;
+            nome.setError("Campo em braco");
+        }
+        return erro;
+
+    }
+
+    private boolean validarTelefone(){
+
+        boolean erro = false;
+        String telefoneString = telefone.getText().toString().trim();
+        if (telefoneString.isEmpty()){
+            erro = true;
+            telefone.setError("Campo em branco");
+        } else if(!telefoneString.matches("[0-9]+")){
+            erro = true;
+            telefone.setError("Telefone não contem apenas numeros");
+        }
+        return erro;
+
+    }
+
 }

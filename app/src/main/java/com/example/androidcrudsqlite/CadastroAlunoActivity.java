50a28c13d8af252de64fba75ef77a49e71ac0ba7
djairb <djair.batista@outlook.com>
2022-10-22 11:30:40 -0300

    criação da classe Aluno e link dos edit texts na tela

--- app/src/main/java/com/example/androidcrudsqlite/CadastroAlunoActivity.java
+++ app/src/main/java/com/example/androidcrudsqlite/CadastroAlunoActivity.java
@@ -3,12 +3,37 @@ package com.example.androidcrudsqlite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CadastroAlunoActivity extends AppCompatActivity {
+
+    private EditText nome;
+    private EditText cpf;
+    private EditText telefone;
+    private Button salvar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_aluno);
+
+        nome = findViewById(R.id.editNomeCampo);
+        cpf = findViewById(R.id.editCpfCampo);
+        telefone = findViewById(R.id.editTelefoneCampo);
+        salvar = findViewById(R.id.botaoSalvar);
+
+        salvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Aluno aluno = new Aluno();
+                aluno.setCpf(cpf.getText().toString());
+                aluno.setNome(nome.getText().toString());
+                aluno.setTelefone(telefone.getText().toString());
+
+            }
+        });
+
     }
 }

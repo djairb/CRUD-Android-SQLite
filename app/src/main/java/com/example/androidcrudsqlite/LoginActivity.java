3c28a75879a5647851e8a08c1990516c9af4510b
djairb <djair.batista@outlook.com>
2022-11-02 15:57:07 -0300

    primeiras alterações para a tela de login

--- app/src/main/java/com/example/androidcrudsqlite/LoginActivity.java
+++ app/src/main/java/com/example/androidcrudsqlite/LoginActivity.java
@@ -3,12 +3,24 @@ package com.example.androidcrudsqlite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText cpfLogin;
+    private EditText senhaLogin;
+    private Button botaoLogar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        cpfLogin = findViewById(R.id.editTextLoginCpf);
+        senhaLogin = findViewById(R.id.editTextLoginSenha);
+        botaoLogar = findViewById(R.id.botaoLogin);
+
+
     }
 }
